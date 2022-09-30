e22b01b891c6d3244c9d20d1c8d5e2b2627fbcf4
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-09-30 10:01:36 -0300

    refactor: using Omit to infer types

--- src/modules/customers/contracts/create-customer.ts
+++ src/modules/customers/contracts/create-customer.ts
@@ -2,16 +2,6 @@ import { Either } from "@/shared/either";
 import { Customer } from "../infra/typeorm/entities/customer";
 
 export namespace CreateCustomer {
-  export type Params = {
-    full_name: string;
-    cpf_cnpj: string;
-    state_registration: number;
-    phone: string;
-    email: string;
-    state: string;
-    city: string;
-    address: string;
-    zip_code: string
-  }
+  export type Params = Omit<Customer, "id" | "created_at">
   export type Result = Either<Error, Customer>
 }
--- src/modules/customers/contracts/updated-customer.ts
+++ src/modules/customers/contracts/updated-customer.ts
@@ -2,17 +2,6 @@ import { Either } from "@/shared/either";
 import { Customer } from "../infra/typeorm/entities/customer";
 
 export namespace UpdateCustomer {
-  export type Params = {
-    id: string
-    full_name: string;
-    cpf_cnpj: string;
-    state_registration: number;
-    phone: string;
-    email: string;
-    state: string;
-    city: string;
-    address: string;
-    zip_code: string
-  }
+  export type Params = Omit<Customer, "created_at">
   export type Result = Either<Error, Customer>
 }
