f4f056711043790b8c00c6e5583d8871c05f3393
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-10-14 10:57:49 -0300

    implements repository to administrator company

--- src/modules/administrator/repositories/administrator-repository.ts
+++ src/modules/administrator/repositories/administrator-repository.ts
@@ -3,7 +3,7 @@ import { Administrator } from "../infra/typeorm/entities/adminitrator"
 
 export interface AdministratorRepository {
   add(data: CreateAdministratorCompany.Params): Promise<Administrator>
-  delete(id: string): Promise<boolean>
+  delete?(id: string): Promise<boolean>
   list(): Promise<Administrator[]>
   update(data: Administrator): Promise<Administrator>
 }
