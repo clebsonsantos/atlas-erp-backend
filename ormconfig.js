820a85e1b579d79ca3698e22518aa452a6427224
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-24 11:12:03 -0300

    resolve connection with database

--- ormconfig.js
+++ ormconfig.js
@@ -7,7 +7,7 @@ module.exports = {
   database: "expense_manager_software",
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
   entities: [
-    `${process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'}/infra/database/entities/index.{js,ts}`
+    `${process.env.TS_NODE_DEV === undefined ? 'dist/src' : 'src'}/infra/database/entities/index.{js,ts}`
   ],
   cli: {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
--- src/infra/database/repositories/administrator-repository-impl.ts
+++ src/infra/database/repositories/administrator-repository-impl.ts
@@ -1,28 +1,32 @@
 import { AdministratorRepository } from "@/domain/contracts/repositories/administrator-repository"
 import { Admin } from "@/domain/entities"
 import { Administrator } from "@/infra/database/entities"
-import { getRepository, Repository } from "typeorm"
+import { getRepository } from "typeorm"
 
 export class AdministratorRepositoryImpl implements AdministratorRepository {
-  private readonly repository: Repository<Administrator>
-  constructor(){
-    this.repository = getRepository(Administrator)
-  }
 
   async add(data: Admin): Promise<Administrator> {
-    const admin = this.repository.create(data)
-    return await this.repository.save(admin)
+    const repository = getRepository(Administrator)
+
+    const admin = repository.create(data)
+    return await repository.save(admin)
   }
 
   async list(): Promise<Administrator[]> {
-    return await this.repository.find()
+    const repository = getRepository(Administrator)
+
+    return await repository.find()
   }
 
   async update(data: Admin): Promise<Administrator> {
-    return await this.repository.save(data)
+    const repository = getRepository(Administrator)
+
+    return await repository.save(data)
   }
 
   async findById(id: string): Promise<Administrator> {
-    return await this.repository.findOne({ id })
+    const repository = getRepository(Administrator)
+
+    return await repository.findOne({ id })
   }
 }
